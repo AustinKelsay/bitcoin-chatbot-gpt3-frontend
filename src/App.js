4ce1118645b927322222b6e976eb7c6d2a5d3f03
austinkelsay <austinkelsay@yahoo.com>
2021-08-14 19:47:37 -0600

    Trying to get request to worb but cors...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import axios from "axios"
 
 const App = () => {
-    msg = "Hello, world!"
-    return <div>{msg}</div>
+    const [botStatus, setBotStatus] = useState("")
+    useEffect(() => {
+        axios.get("https://bitcoin-chatbot-gpt3-1.koie11.repl.co/")
+        .then(response => {
+            console.log(response)
+            setBotStatus(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    })
+    return <div>{botStatus}</div>
 }
 
 export default App
