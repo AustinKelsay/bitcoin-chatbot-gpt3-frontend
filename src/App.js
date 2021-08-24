8232531746da299e846f05a31d1774ab5853696b
austinkelsay <austinkelsay@yahoo.com>
2021-08-23 18:13:39 -0600

    Well its broken and ugly but at least it works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, {useEffect, useState} from "react"
 import axios from "axios"
-import { Widget } from 'react-chat-widget';
-import 'react-chat-widget/lib/styles.css';
+import Chat, { Message } from 'react-simple-chat';
+import 'react-simple-chat/src/components/index.css';
 import './index.css'
 
 const App = () => {
     const [botStatus, setBotStatus] = useState("Bot offline")
-    const [messages, setMessages] = useState(["Hey fuckface"])
-    const [isTyping, setIsTyping] = useState(false)
+    const [messages, setMessages] = useState([
+        {
+            id: 1,
+            text: 'Hello my friend!',
+            createdAt: '2021-07-21 12:09:12', // optional
+            user: {
+                id: 2,
+                avatar: 'https://link-to-avatar/avatar.jpg' // optional
+            }
+        }
+    ]);
+
     const headers = {
         'Content-Type': 'application/json',
         "Access-Control-Allow-Origin": "*"
       };
-
-
     useEffect(() => {
         axios.get("https://bitcoin-chatbot-gpt3-1.koie11.repl.co/", {headers})
         .then(response => {
@@ -31,7 +39,12 @@ const App = () => {
                 <span>{botStatus}</span>
             </header>
             <div className='body'>
-                <Widget />
+                <Chat
+                title="Jane Doe"
+                user={{ id: 1 }}
+                messages={messages}
+                onSend={message => setMessages([...messages, message])}
+                />
             </div>
         </div>
         )
