7e085eec1cfef0c0a17341bf75e5ab4b81fc4112
austinkelsay <austinkelsay@yahoo.com>
2021-08-30 07:06:01 -0600

    Got header and onld styles back in, also got chatwindow widthe working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import ThemedExample from './chatbot';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <ThemedExample />
+  const [botStatus, setBotStatus] = useState('Offline');
+  useEffect(() => {
+    const headers = {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*"
+      };
+    axios.get("https://bitcoin-chatbot-gpt3-1.koie11.repl.co/", {headers})
+    .then(response => {
+        setBotStatus(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+
+ 
+return (
+    <div className='App'>
+        <header className='header'>
+            <h1>Bitcoin Chatbot</h1>
+            Status:
+            <span>{botStatus}</span>
+        </header>
+        <ThemedExample />
     </div>
-  );
+    )
 }
 
 export default App;
--- src/chatbot.js
+++ src/chatbot.js
@@ -4,6 +4,7 @@ import ChatBot from 'react-simple-chatbot';
 
 // all available props
 const theme = {
+  margin: '1% auto',
   background: '#f5f8fb',
   fontFamily: 'Helvetica Neue',
   headerBgColor: '#EF6C00',
@@ -15,6 +16,12 @@ const theme = {
   userFontColor: '#4a4a4a',
 };
 
+const styles = {
+  'margin': '1% auto',
+  'marginBottom': '0%',
+  'marginTop': '10%'
+}
+
 const steps = [
   {
     id: '1',
@@ -25,7 +32,11 @@ const steps = [
 
 const ThemedExample = () => (
   <ThemeProvider theme={theme}>
-    <ChatBot steps={steps} />
+    <ChatBot 
+      style={styles}
+      width={'90%'}
+      steps={steps} 
+    />
   </ThemeProvider>
 );
 
