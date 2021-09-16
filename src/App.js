bfb09686413cc374bf3d697f231e462f4172afdf
austinkelsay <austinkelsay@yahoo.com>
2021-09-16 11:09:24 -0600

    Cleaned things up but state still isn't updating right

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import ThemedExample from './chatbot';
+import Chatbot from './chatbot';
 import './App.css';
 import StepCreator from './stepCreator';
 
 function App() {
   const [botStatus, setBotStatus] = useState('Offline');
-  const [chatLog, setChatLog] = useState('');
-  const [steps, setSteps] = useState(
-    [
-        {
-            id: 1,
-            message: 'Hello World!',
-            trigger: 2
-        },
-        {
-            id: 2,
-            message: "I'm Bitcoin Chatbot",
-            trigger: 3
-        },
-        {
-            id: 3,
-            message: "What can I answer for you?",
-            trigger: 4
-        },
-        {
-            id: 4,
-            user: true,
-            trigger: 5
-        },
-        {
-            id: 5,
-            message: ({ previousValue, steps }) => {
-                setTimeout(() => {
-                    postSteps(previousValue)
-                }, 1000)
-                // Set chatlog from previous value
-                // pass prev value into func that will remove this step and create a new bot step followed by a uesr step to respond
-            },
-            end: true
-        }
-]);
+  
   useEffect(() => {
     const headers = {
         'Content-Type': 'application/json',
@@ -53,49 +19,11 @@ function App() {
     .catch(error => {
         console.log(error)
     })
-});
-
-const postSteps = (chat) => {
-    const headers = {
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "*"
-      };
-    console.log(chat)
-    axios.post("https://bitcoin-chatbot-gpt3-1.koie11.repl.co/ask", {question: chat}, {headers})
-    .then(response => {
-        if (response.data != null) {
-            createStep(response.data)
-            }
-        })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-// Step creation:
-// If type is user, then create user step with a step afterwords that passes steps into postSteps
-// If type is bot, then create bot step by invoking stepCreator from the response of postSteps and then invoke stepCreator again with user true
-
-const createStep = (newStepText) => {
-    let step_copy = steps
-    let last_step = step_copy.pop()
-    const bot_Step = {
-        id: last_step.id + 1,
-        message: newStepText,
-        trigger: last_step.id + 2
-    }
-    const following_user_step = {
-        id: last_step.id + 2,
-        user: true,
-        end: true
-    }
-    setSteps([...steps, bot_Step, following_user_step])
-
-    return 
-}
+},[]);
  
 return (
     <div className='App'>
+        {console.log('here')}
         <header className='header'>
             <h1>Bitcoin Chatbot</h1>
             Status:
@@ -113,7 +41,7 @@ return (
                 </section>
             </div>
         </header>
-        <ThemedExample steps={steps} />
+        <Chatbot />
     </div>
     )
 }
--- src/chatbot.js
+++ src/chatbot.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { ThemeProvider } from 'styled-components';
 import ChatBot from 'react-simple-chatbot';
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios'
 
 // all available props
 const theme = {
@@ -32,4 +32,75 @@ const ThemedExample = ({steps}) => (
   </ThemeProvider>
 );
 
-export default ThemedExample;
+const Chatbot = () => {
+  const [steps, setSteps] = useState(
+    [
+        {
+            id: 1,
+            message: 'Hello World!',
+            trigger: 2
+        },
+        {
+            id: 2,
+            message: "I'm Bitcoin Chatbot",
+            trigger: 3
+        },
+        {
+            id: 3,
+            message: "What can I answer for you?",
+            trigger: 4
+        },
+        {
+            id: 4,
+            user: true,
+            trigger: 5
+        },
+        {
+            id: 5,
+            message: ({ previousValue, steps }) => {
+                postSteps(previousValue)
+                // Set chatlog from previous value
+                // pass prev value into func that will remove this step and create a new bot step followed by a uesr step to respond
+            },
+            delay: 5000,
+            end: true
+        }
+  ]);
+
+  const postSteps = (chat) => {
+    const headers = {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*"
+      };
+    axios.post("https://bitcoin-chatbot-gpt3-1.koie11.repl.co/ask", {question: chat}, {headers})
+    .then(response => {
+      createStep(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+  }
+
+  const createStep = (newStepText) => {
+    console.log(newStepText)
+    let step_copy = steps
+    let last_step = step_copy.pop()
+    const bot_Step = {
+        id: last_step.id + 1,
+        message: newStepText,
+        trigger: last_step.id + 2
+    }
+    const following_user_step = {
+        id: last_step.id + 2,
+        user: true,
+        end: true
+    }
+
+    const newSteps = [...step_copy, bot_Step, following_user_step]
+    setSteps(newSteps)
+  }
+
+  return <ThemedExample steps={steps} />
+}
+
+export default Chatbot;
